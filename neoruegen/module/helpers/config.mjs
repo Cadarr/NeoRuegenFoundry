79ea152b8462f9756a6f9bd3b3d239aafa617798
Thorsten Diekhof <kontakt@thorsten-diekhof.de>
2026-05-18 11:59:19 +0200

    first commit

---
+++ neoruegen/module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const NEORUEGEN = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+NEORUEGEN.abilities = {
+  str: 'NEORUEGEN.Ability.Str.long',
+  dex: 'NEORUEGEN.Ability.Dex.long',
+  con: 'NEORUEGEN.Ability.Con.long',
+  int: 'NEORUEGEN.Ability.Int.long',
+  wis: 'NEORUEGEN.Ability.Wis.long',
+  cha: 'NEORUEGEN.Ability.Cha.long',
+};
+
+NEORUEGEN.abilityAbbreviations = {
+  str: 'NEORUEGEN.Ability.Str.abbr',
+  dex: 'NEORUEGEN.Ability.Dex.abbr',
+  con: 'NEORUEGEN.Ability.Con.abbr',
+  int: 'NEORUEGEN.Ability.Int.abbr',
+  wis: 'NEORUEGEN.Ability.Wis.abbr',
+  cha: 'NEORUEGEN.Ability.Cha.abbr',
+};
---
+++ neoruegen/src/datamodels/module/data/base-item.mjs
@@ -0,0 +1,14 @@
+import NeoruegenDataModel from "./base-model.mjs";
+
+export default class NeoruegenItemBase extends NeoruegenDataModel {
+
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    const schema = {};
+
+    schema.description = new fields.StringField({ required: true, blank: true });
+
+    return schema;
+  }
+
+}
